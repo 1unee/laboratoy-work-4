0db282eae4c88059714a13e4dd66351ee64bb4a4
1unee <>
2024-05-31 20:46:53 +0300

    develop : fix problem on not mapping id as field from superclass by modelmapper

--- src/main/java/com/oneune/laboratory/work/configs/AppConfig.java
+++ src/main/java/com/oneune/laboratory/work/configs/AppConfig.java
@@ -4,11 +4,17 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import static org.modelmapper.config.Configuration.AccessLevel.PRIVATE;
+
 @Configuration
 public class AppConfig {
 
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setFieldMatchingEnabled(true)
+                .setFieldAccessLevel(PRIVATE);
+        return modelMapper;
     }
 }
